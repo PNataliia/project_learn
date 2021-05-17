1ec58c49874c19d288bc7106f5895e6a99fc99f2
Natalia <natalja.bukavyn@gmail.com>
2021-05-17 17:20:36 +0200

    cycle

--- js/main.js
+++ js/main.js
@@ -32,23 +32,54 @@
 //     console.log("er");
 // }
 
-const numb = 50;
- (numb === 50) ? console.log("ok") : console.log("er");
+// const numb = 50;
+//  (numb === 50) ? console.log("ok") : console.log("er");
 
- //swich
- const numbSwich = 50;
+//  //swich
+//  const numbSwich = 50;
 
- switch (numbSwich){
-    case 49: 
-        console.log("er");
-        break;
-    case 51: 
-        console.log("er");
-        break;
-    case 100: 
-        console.log("er");
-        break;
-    default:
-        console.log("not to you");
-        break;
- }
+//  switch (numbSwich){
+//     case 49: 
+//         console.log("er");
+//         break;
+//     case 51: 
+//         console.log("er");
+//         break;
+//     case 100: 
+//         console.log("er");
+//         break;
+//     default:
+//         console.log("not to you");
+//         break;
+//  }
+
+// cycle
+let numb1 = 50;
+let numb2 = 70;
+let numb3 = 50;
+//first examle
+    while(numb1 < 55){
+        console.log(numb1);
+        numb1++;
+    }
+//second examle
+    do {
+        console.log(numb2);
+        numb2++;
+    }
+    while(numb2 < 75)
+
+//third examle 
+for (let i = 1; i < 10; i++){
+    if (i===6){
+        break;//break cycle
+    }
+    console.log(numb3);
+}
+
+for (let i = 1; i < 10; i++){
+    if (i===6){
+        continue;//break cycle only i===6 and continue
+    }
+    console.log(numb3);
+}
