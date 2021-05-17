bf55e159c9f2c544391b2b7e5ec63c91a2a50d7b
Natalia <natalja.bukavyn@gmail.com>
2021-05-17 14:52:45 +0200

    create object

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@
 // alert(`Привет, ${user}`);
 
 //9
+
+const numberOfFilms = +prompt('скільки фільмів ви вже переглянули', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: {},
+    privat: false
+};
+const a = prompt('Один із переглянутих фільмів', ''),
+        b = prompt('Як ви його оціните', ''),
+        c = prompt('Один із останніх переглянутих фільмів', ''),
+        d = prompt('Як ви його оціните', ''); 
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
