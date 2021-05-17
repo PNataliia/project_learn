b8b426a7e3569db29100c278e3ba66ed800f42dd
Natalia <natalja.bukavyn@gmail.com>
2021-05-17 16:25:43 +0200

    condition and switch

--- js/main.js
+++ js/main.js
@@ -7,21 +7,48 @@
 
 //9
 
-const numberOfFilms = +prompt('скільки фільмів ви вже переглянули', '');
+// const numberOfFilms = +prompt('скільки фільмів ви вже переглянули', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    generes: {},
-    privat: false
-};
-const a = prompt('Один із переглянутих фільмів', ''),
-        b = prompt('Як ви його оціните', ''),
-        c = prompt('Один із останніх переглянутих фільмів', ''),
-        d = prompt('Як ви його оціните', ''); 
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     generes: {},
+//     privat: false
+// };
+// const a = prompt('Один із переглянутих фільмів', ''),
+//         b = prompt('Як ви його оціните', ''),
+//         c = prompt('Один із останніх переглянутих фільмів', ''),
+//         d = prompt('Як ви його оціните', ''); 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if(4==9){
+//     console.log("ok");
+// }else{
+//     console.log("er");
+// }
+
+const numb = 50;
+ (numb === 50) ? console.log("ok") : console.log("er");
+
+ //swich
+ const numbSwich = 50;
+
+ switch (numbSwich){
+    case 49: 
+        console.log("er");
+        break;
+    case 51: 
+        console.log("er");
+        break;
+    case 100: 
+        console.log("er");
+        break;
+    default:
+        console.log("not to you");
+        break;
+ }
