92de683a0f0b4121478cb92e8712e243e9e70603
Natalia <natalja.bukavyn@gmail.com>
2021-05-17 18:31:53 +0200

    cycle example

--- js/main.js
+++ js/main.js
@@ -7,24 +7,45 @@
 
 //9
 
-// const numberOfFilms = +prompt('скільки фільмів ви вже переглянули', '');
+const numberOfFilms = +prompt('скільки фільмів ви вже переглянули', '');
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     generes: {},
-//     privat: false
-// };
-// const a = prompt('Один із переглянутих фільмів', ''),
-//         b = prompt('Як ви його оціните', ''),
-//         c = prompt('Один із останніх переглянутих фільмів', ''),
-//         d = prompt('Як ви його оціните', ''); 
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: {},
+    privat: false
+};
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один із переглянутих фільмів', ''),
+        b = prompt('Як ви його оціните', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else{
+            console.log('error');
+            i--;//restart cycle
+        }
+        
+}
+if (personalMovieDB.count < 10){
+    console.log('few movies');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('more movies');
+}else if (ersonalMovieDB.count >= 30){
+    console.log('melomaniac');
+}else{
+    console.log('error'); 
+}
 
-// console.log(personalMovieDB);
+
+
+
+
+
+
+
 
 // if(4==9){
 //     console.log("ok");
@@ -53,33 +74,33 @@
 //         break;
 //  }
 
-// cycle
-let numb1 = 50;
-let numb2 = 70;
-let numb3 = 50;
-//first examle
-    while(numb1 < 55){
-        console.log(numb1);
-        numb1++;
-    }
-//second examle
-    do {
-        console.log(numb2);
-        numb2++;
-    }
-    while(numb2 < 75)
+// // cycle
+// let numb1 = 50;
+// let numb2 = 70;
+// let numb3 = 50;
+// //first examle
+//     while(numb1 < 55){
+//         console.log(numb1);
+//         numb1++;
+//     }
+// //second examle
+//     do {
+//         console.log(numb2);
+//         numb2++;
+//     }
+//     while(numb2 < 75)
 
-//third examle 
-for (let i = 1; i < 10; i++){
-    if (i===6){
-        break;//break cycle
-    }
-    console.log(numb3);
-}
+// //third examle 
+// for (let i = 1; i < 10; i++){
+//     if (i===6){
+//         break;//break cycle
+//     }
+//     console.log(numb3);
+// }
 
-for (let i = 1; i < 10; i++){
-    if (i===6){
-        continue;//break cycle only i===6 and continue
-    }
-    console.log(numb3);
-}
+// for (let i = 1; i < 10; i++){
+//     if (i===6){
+//         continue;//break cycle only i===6 and continue
+//     }
+//     console.log(numb3);
+// }
