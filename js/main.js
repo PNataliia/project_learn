c2f2f14bac10229a84e51ad935dc865351cb3a63
Natalia <natalja.bukavyn@gmail.com>
2021-05-18 21:03:42 +0200

    array  and object

--- js/main.js
+++ js/main.js
@@ -1,106 +1,86 @@
 "use strict";
-// //8
-// const category = "toys";
-// console.log(`https://someurl.com/${category}/5`);
-// const user = "Ivan";
-// alert(`Привет, ${user}`);
+ //22
+ let a = 5,
+    b = a;
 
-//9
+    b = b + 5;
 
-const numberOfFilms = +prompt('скільки фільмів ви вже переглянули', '');
+    
+    console.log(b);
+    console.log(a);
 
+    const obj = {
+        a: 5,
+        b: 1
+    };
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    generes: {},
-    privat: false
-};
-for (let i = 0; i < 2; i++){
-    const a = prompt('Один із переглянутих фільмів', ''),
-        b = prompt('Як ви його оціните', '');
-        if (a != null && b != null && a != '' && b != '' && a.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else{
-            console.log('error');
-            i--;//restart cycle
+    // const copy =  obj;//link(ссилка)
+    
+    // copy.a = 10;
+
+    // console.log(copy);
+    // console.log(obj)
+
+    function copy(mainObj){
+        let objCopy = {};
+        let key;
+        for(key in mainObj){
+            objCopy[key] = mainObj[key];
         }
-        
-}
-if (personalMovieDB.count < 10){
-    console.log('few movies');
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log('more movies');
-}else if (ersonalMovieDB.count >= 30){
-    console.log('melomaniac');
-}else{
-    console.log('error'); 
-}
+
+        return objCopy;
+    }
+
+    const numbers = {
+        a: 2,
+        b: 5,
+        c: {
+            x: 7,
+            y: 4
+        }
+    }
+
+    const newNumbers = copy(numbers);//clone object
+    newNumbers.a = 10;
+    newNumbers.c.x = 10; //обєкт в обєкті не копіюється. поверхневе копіювання обєктів
 
 
+    const add = {
+        d: 17,
+        e: 20
+    }
 
+    const clone = (Object.assign(numbers, add));
 
+    clone.d = 20;
 
+    console.log(add);
+    console.log(clone);
 
+     const oldArray = ['a', 'b', 'c'];
+     const newArray = oldArray.slice();
 
+     newArray[1] = 'sdgsdg';
+    console.log(oldArray);
+    console.log(newArray);
 
+    //ES6 ES8
 
-// if(4==9){
-//     console.log("ok");
-// }else{
-//     console.log("er");
-// }
+    //spread orator
+    const video = ['youtube', 'vimeo', 'rutube'],
+          blogs = ['wordpress', 'livejournal', 'blogger'],
+          internet = [...video, ...blogs, 'vk', 'instagram'];
 
-// const numb = 50;
-//  (numb === 50) ? console.log("ok") : console.log("er");
+    console.log(internet);
 
-//  //swich
-//  const numbSwich = 50;
+    const array = ['a', 'b'];
 
-//  switch (numbSwich){
-//     case 49: 
-//         console.log("er");
-//         break;
-//     case 51: 
-//         console.log("er");
-//         break;
-//     case 100: 
-//         console.log("er");
-//         break;
-//     default:
-//         console.log("not to you");
-//         break;
-//  }
+    const newAaray = [...array];
 
-// // cycle
-// let numb1 = 50;
-// let numb2 = 70;
-// let numb3 = 50;
-// //first examle
-//     while(numb1 < 55){
-//         console.log(numb1);
-//         numb1++;
-//     }
-// //second examle
-//     do {
-//         console.log(numb2);
-//         numb2++;
-//     }
-//     while(numb2 < 75)
+    const q = {
+        one: 1,
+        two: 2
+    }
 
-// //third examle 
-// for (let i = 1; i < 10; i++){
-//     if (i===6){
-//         break;//break cycle
-//     }
-//     console.log(numb3);
-// }
+    const newObj = {...q};
 
-// for (let i = 1; i < 10; i++){
-//     if (i===6){
-//         continue;//break cycle only i===6 and continue
-//     }
-//     console.log(numb3);
-// }
